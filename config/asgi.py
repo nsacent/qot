f7f2633f83fab9187bbe12eb1c483961a717de1d
Vimbah Computers <vimbahcomputers@Vimbahs-iMac.local>
2026-06-04 03:01:27 -0700

    Add JWT authentication for websocket chat

--- config/asgi.py
+++ config/asgi.py
@@ -6,16 +6,16 @@ from django.core.asgi import get_asgi_application
 
 django_asgi_app = get_asgi_application()
 
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
+from apps.chats.middleware import JWTAuthMiddlewareStack
 from config.routing import websocket_urlpatterns
 
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(
+        "websocket": JWTAuthMiddlewareStack(
             URLRouter(websocket_urlpatterns)
         ),
     }
