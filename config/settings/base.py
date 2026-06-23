c71e7311f7b8757b87ce575e191eb5d04965009c
Vimbah Computers <vimbahcomputers@Vimbahs-iMac.local>
2026-06-23 00:26:33 -0700

    Add custom API pagination

--- config/settings/base.py
+++ config/settings/base.py
@@ -146,7 +146,7 @@ REST_FRAMEWORK = {
         "rest_framework.filters.OrderingFilter",
     ],
 
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS": "apps.common.pagination.StandardResultsSetPagination",
     "PAGE_SIZE": 20,
 }
 
